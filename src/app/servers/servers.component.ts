dca6e8cc60fc9263cb19e42747423b84c4eebb14
Abhishek <avish121@gmail.com>
2022-07-07 22:42:17 +0530

    minor changes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,14 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  allowedAddButton: Boolean = false
+  allowedAddButton: Boolean = false;
+  serverName:string = "";
+  serverCreatedStatus: string = "No server created";
   constructor() {
     setTimeout(()=> {
       this.allowedAddButton = true
-    },5000)
+    },2000)
   }
 
   ngOnInit(): void {
   }
+
+  onUpdateServerName(event: Event){
+    //console.log(event.target);
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
+
+  onCreateServer(){
+    this.serverCreatedStatus = "Server is created, name is " + this.serverName;
+  }
+
 
 }
